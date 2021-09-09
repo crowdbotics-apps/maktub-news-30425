2447f88ca95e14d8a700fb9c1b572b35217aeeae
Crowdbotics <team@crowdbotics.com>
2021-09-09 23:40:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "maktub_news_30425Identifier",
+  url: "https://maktub_news_30425Identifier.botics.co",
+  api: "https://maktub_news_30425Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
